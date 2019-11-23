cc6ce1898ea8145d20a6e69a708c69d06b444b46
Mustafa Sabri Oğuz <m.sabri.oguz@gmail.com>
2019-11-23 09:26:33 +0300

    Matematik İşlemleri - Çıkarma

---
+++ Temel Dersler/aritmetik-islemler.py
@@ -0,0 +1,19 @@
+sayi1 = 1453
+sayi2 = 547
+
+sonuc = sayi1 + sayi2
+
+print("Sonuç değeri : ", sonuc)
+
+# Çıktı Sonucu
+# Sonuç değeri : 2000
+
+sayi3 = 3043
+sayi4 = 1500
+
+sonuc = sayi3 - sayi4
+
+print("Sonuç değeri : ", sonuc)
+
+# Çıktı Sonucu
+# Sonuç değeri : 1543
