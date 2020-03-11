fcda7851082fa44efa417b6ef96451dcbcaf0f59
Mustafa Oğuz <iyimser.tuzlali@hotmail.com>
2020-03-11 08:29:56 +0300

    Python Aritmetik Operatörler - Dersi Eklendi.

---
+++ Temel Dersler/Aritmetik Islemler/islem-onceligi.py
@@ -0,0 +1,8 @@
+## Operatörlerin işlem önceliği
+## 165 / (31 - 26) x 3 işleminin sonucu kaçtır?
+
+sonuc = 165 / (31 - 26) * 3
+print("İşlemin Sonucu: ", sonuc)
+
+# Çıktı Sonucu
+# İşlemin Sonucu:  99.0
--- Temel Dersler/Aritmetik İslemler/aritmetik-islemler.py
+++
@@ -1,2 +0,0 @@
-## Aritmetik Problemler
-### Problem : 
